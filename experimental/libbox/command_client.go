2da16edd90b8130978240b4446335060db39661b
Ebrahim Tahernejad <ebrahimtahernejad@gmail.com>
2024-04-25 22:53:36 +0330

    Added dial port to command client
    Signed-off-by: Ebrahim Tahernejad <ebrahimtahernejad@gmail.com>

--- experimental/libbox/command_client.go
+++ experimental/libbox/command_client.go
@@ -51,7 +51,7 @@ func (c *CommandClient) directConnect() (net.Conn, error) {
 			Net:  "unix",
 		})
 	} else {
-		return net.Dial("tcp", "127.0.0.1:8964")
+		return net.Dial("tcp", "127.0.0.1:" + string(sTVOSPort))
 	}
 }
 
